893e8817323bb1de56927fa223765e9d073edd88
krharsh17 <kharsh39@gmail.com>
2020-03-28 03:47:50 +0530

    Bug fixes

---
+++ precache-manifest.29a2974b96d8c6841b94b1ffddc61eff.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3576382ef98a6ee5ddbf452f49ded8cb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8c9c573fb6f62e8d8377",
+    "url": "/static/css/main.d877a721.chunk.css"
+  },
+  {
+    "revision": "0ff8f0b7b8dad3640709",
+    "url": "/static/js/2.33c03888.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.33c03888.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8c9c573fb6f62e8d8377",
+    "url": "/static/js/main.57ea599b.chunk.js"
+  },
+  {
+    "revision": "fb35f373ff70a41c9adb",
+    "url": "/static/js/runtime-main.eea84414.js"
+  },
+  {
+    "revision": "dba5b8bdbf10623f8a12e673a7ee5ddb",
+    "url": "/static/media/ic-resume.dba5b8bd.svg"
+  },
+  {
+    "revision": "1f546ea62732fec09ab05b74411fd2b0",
+    "url": "/static/media/main-bg.1f546ea6.svg"
+  },
+  {
+    "revision": "0fe7ba9261e9e54f386bee0420338f34",
+    "url": "/static/media/wip.0fe7ba92.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.54a8960b2c29c434aea98ff92a671aa6.js"
+  "/precache-manifest.29a2974b96d8c6841b94b1ffddc61eff.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
